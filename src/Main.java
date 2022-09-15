bf8883f6f74a8cecd3f0e766528ebe9231bb6ddd
Julia Khramova <infiniti211184@mail.ru>
2022-09-15 17:22:08 +0300

    Домашняя работа, исправлена опечатка

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+
     public static void main(String[] args) {
         //адание 1.
         int[] array1 = new int[3];
@@ -13,39 +14,42 @@ public class Main {
     }
 }
 // Задание 2.
-        for (int i = 0; i < array1.length; i++){
+        for(int i=0;i<array1.length;i++){
         System.out.println(array1[i]);
-        if(i!=array1.length-1) {
+        if(i!=array1.length-1){
         System.out.println(", ");
         }
         }
         System.out.prinln();
-        for (int i = 0; i < array2.length; i++){
+        for(int i=0;i<array2.length;i++){
         System.out.println(array1[i]);
-        if(i!=array2.length-1) {
+        if(i!=array2.length-1){
         System.out.println(", ");
         }
         }
         System.out.prinln();
-        for (int i = 0; i < array3.length; i++){
+        for(int i=0;i<array3.length;i++){
         System.out.println(array1[i]);
-        if(i!=array3.length-1) {
+        if(i!=array3.length-1){
         System.out.println(", ");
         }
         }
 
 
         //задание 3.
-        for (int i = array1.length-1; i >=0; i++) {
+        for(int i=array1.length-1;i>=0;i++){
         System.out.print(array1[i]);
-        if (i != 0) {
+        if(i!=0){
         System.out.println(", ");
         }
         }
         System.out.println();
-        for (int i = array2.length-1; i >= 0; i++) {
+        for(int i=array2.length-1;i>=0;i++){
         System.out.print(array2[i]);
         }
+        }
+        }
+
 
 
 
