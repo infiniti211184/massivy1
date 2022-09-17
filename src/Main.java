c27ecc7e27710396dc800381c7f5c97573eaad6a
Julia Khramova <infiniti211184@mail.ru>
2022-09-17 18:02:54 +0300

    Домашняя работа Методы,справлены ошибки.

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,72 @@
 public class Main {
-
     public static void main(String[] args) {
         //адание 1.
         int[] array1 = new int[3];
         array1[0] = 1;
         array1[1] = 2;
         array1[2] = 3;
-        for (int i = 0; i < array1.length; i++) ;
-
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = i + 1;
+        }
         double[] array2 = {1.57, 7.654, 9.986};
         boolean[] array3 = {true, false};
+
+// Задание 2.
+        for (int i = 0; i < array1.length; i++) {
+            System.out.print(array1[i]);
+            if (i != array1.length - 1) {
+                System.out.print(", ");
+            }
+        }
         System.out.println();
+        for (int i = 0; i < array2.length; i++) {
+            System.out.print(array1[i]);
+            if (i != array2.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < array3.length; i++) {
+            System.out.print(array3[i]);
+            if (i != array3.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        // адание 3.
+        for (int i = array1.length - 1; i >= 0; i--) {
+            System.out.print(array1[i]);
+            if (i != 0) {
+                System.out.println(", ");
+            }
+        }
+        System.out.println();
+        for (int i = array2.length - 1; i >= 0; i--) {
+            System.out.print(array2[i]);
+            if (i != 0) {
+                System.out.println(", ");
+            }
+        }
+        System.out.println();
+        for (int i = array3.length - 1; i <= 0; i--) {
+            System.out.print(array3[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        // Зфдание 4.
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] % 2 == 1) {
+                array1[i]++;
+            }
+            System.out.print(array1[i]);
+            if (i != array1.length - 1) {
+                System.out.print(", ");
+            }
+        }
     }
 }
-// Задание 2.
-        for(int i=0;i<array1.length;i++){
-        System.out.println(array1[i]);
-        if(i!=array1.length-1){
-        System.out.println(", ");
-        }
-        }
-        System.out.prinln();
-        for(int i=0;i<array2.length;i++){
-        System.out.println(array1[i]);
-        if(i!=array2.length-1){
-        System.out.println(", ");
-        }
-        }
-        System.out.prinln();
-        for(int i=0;i<array3.length;i++){
-        System.out.println(array1[i]);
-        if(i!=array3.length-1){
-        System.out.println(", ");
-        }
-        }
 
-
-        //задание 3.
-        for(int i=array1.length-1;i>=0;i++){
-        System.out.print(array1[i]);
-        if(i!=0){
-        System.out.println(", ");
-        }
-        }
-        System.out.println();
-        for(int i=array2.length-1;i>=0;i++){
-        System.out.print(array2[i]);
-        }
-        }
-        //задание 4.
-// Задание 4.
-        for (int i = 0; i < array1.lenght; i++){
-        if(array1[i]%2==1){
-        array[i]++;  //  программировании четн/нечет при делении на 2 ==1
-        }
-        System.out.print(array1[i]);
-        if(i!=array1.length-1){
-        System.out.print(", ");
-        }
-        }
-        }
-        }
 
 
 
